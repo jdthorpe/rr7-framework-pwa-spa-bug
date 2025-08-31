aaf2fc6b1f550f735ccf59fac20eef78c43e72a3
Jason Thorpe <jdthorpe@gmail.com>
2025-08-31 10:25:44 -0700

    adding prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,9 @@ import type {Config} from "@react-router/dev/config";
 
 export default {
   // Config options...
+  /* UNCOMMENT TO PRERENDER THE ROOT ROUTE
+  prerender: ["/"],
+  */
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
